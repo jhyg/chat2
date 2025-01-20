1ac0066799c548d88628fb7e232435ac40b71b6b
jhyg <65217813+jhyg@users.noreply.github.com>
2025-01-20 18:01:53 +0900

    commit

---
+++ frontend/src/components/EnterChatRoomCommand.vue
@@ -0,0 +1,65 @@
+<template>
+    <v-card outlined>
+        <v-card-title>
+            EnterChatRoom
+        </v-card-title>
+
+        <v-card-text>
+            <String label="UserId" v-model="value.userId" :editMode="editMode"/>
+            <String label="UserName" v-model="value.userName" :editMode="editMode"/>
+            <String label="UserPassWord" v-model="value.userPassWord" :editMode="editMode"/>
+        </v-card-text>
+
+        <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+                    color="primary"
+                    text
+                    @click="enterChatRoom"
+            >
+                EnterChatRoom
+            </v-btn>
+            
+            <v-btn
+                    color="primary"
+                    text
+                    @click="close"
+            >
+                Close
+            </v-btn>
+        </v-card-actions>
+    </v-card>
+
+</template>
+
+<script>
+   
+    export default {
+        name: 'EnterChatRoomCommand',
+        components:{},
+        props: {},
+        data: () => ({
+            editMode: true,
+            value: {},
+        }),
+        created() {
+            this.value.userId = '';
+            this.value.userName = '';
+            this.value.userPassWord = '';
+        },
+        watch: {
+        },
+        methods: {
+            enterChatRoom() {
+                this.$emit('enterChatRoom', this.value);
+            },
+            close() {
+                this.$emit('closeDialog');
+            },
+            change() {
+                this.$emit('input', this.value);
+            },
+        }
+    }
+</script>
+
